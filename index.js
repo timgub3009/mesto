d2b8f13f516d90f49ea16ca6a1c18eded8189aa2
Gubeidulin Timur <timur3009@yandex.ru>
2022-11-11 02:56:22 +0300

    fix form problem

--- index.js
+++ index.js
@@ -3,27 +3,28 @@ const popupCloseBtnElement = popupElement.querySelector('.popup__close-button');
 const popupOpenBtnElement = document.querySelector('.profile__edit-button');
 
 const openPopup = function(event) {
-  popupElement.classList.add('popup_is-on');
+  popupElement.classList.add('popup_opened');
 }
 
 const closePopup = function() {
-  popupElement.classList.remove('popup_is-on');
+  popupElement.classList.remove('popup_opened');
 }
 
 popupCloseBtnElement.addEventListener('click', closePopup);
 popupOpenBtnElement.addEventListener('click', openPopup);
 
-let formElement = popupElement.querySelector('.popup__form')
+let formElement = popupElement.querySelector('.popup__form');
 let nameInput = popupElement.querySelector('.popup__input_name_type');
 let jobInput = popupElement.querySelector('.popup__input_job_type');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
 
 function formSubmitHandler (evt) {
-    evt.preventDefault();
-
-nameInput.textContent = nameInput.value;
-jobInput.textContent = jobInput.value;
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  openPopup();
+  closePopup();
 }
-
 formElement.addEventListener('submit', formSubmitHandler);
 
-console.log(formElement);
