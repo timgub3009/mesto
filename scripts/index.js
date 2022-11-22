9a90af1828f2edc8ff18356e726438440d317d89
Gubeidulin Timur <timur3009@yandex.ru>
2022-11-22 21:07:58 +0300

    buttons, minor changes

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,48 @@ const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
 const nameInput = popupElement.querySelector('.popup__input_type_name');
 const jobInput = popupElement.querySelector('.popup__input_type_job');
+const likeBtnElement = document.querySelector('.elements__like-button');
+const popupAddBtnElement = document.querySelector('.profile__add-button');
+const cardNameInput = popupElement.querySelector('.popup__input_type_place');
+const cardLinkInput = popupElement.querySelector('.popup__input_type_link');
+const cardName = document.querySelector('.elements__card-heading');
+const deleteBtnElement = document.querySelector('.elements__delete-button');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const likeCounter = function () {
+  likeBtnElement.classList.toggle('elements__like-button_type_active');
+}
+
+const deleteButton = function (event) {
+  const cardElement = event.target.closest('.elements__card');
+  cardElement.remove();
+}
 
 const openPopup = function () {
   popupElement.classList.add('popup_opened');
@@ -34,3 +76,5 @@ popupCloseBtnElement.addEventListener('click', closePopup);
 popupOpenBtnElement.addEventListener('click', openPopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
 formElement.addEventListener('submit', formSubmitHandler);
+likeBtnElement.addEventListener('click', likeCounter);
+deleteBtnElement.addEventListener('click', deleteButton);
